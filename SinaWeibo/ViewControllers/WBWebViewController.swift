1a09b55661df23c2d473f38e72b1e2b7caa894c9
cxjwin <88cxjwin@gmail.com>
2014-06-13 10:39:59 +0800

    update pod file

---
+++ SinaWeibo/ViewControllers/WBWebViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WBWebViewController.swift
+//  SinaWeibo
+//
+//  Created by cxjwin on 6/13/14.
+//  Copyright (c) 2014 cxjwin. All rights reserved.
+//
+
+import UIKit
+
+class WBWebViewController: UIViewController {
+
+	@IBOutlet var webView : UIWebView
+	
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
